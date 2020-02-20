ecb978c49129825c4a98af4cf35292c55757db2b
mjjalala <mohamed.j.j.1988@gmail.com>
2020-02-20 09:34:36 +0000

    End Section Seven

--- ZwajApp-SPA/src/app/nav/nav.component.ts
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/Auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +10,15 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
   model :any={};
-  constructor(public authService:AuthService, private alertify:AlertifyService) { }
+  constructor(public authService:AuthService, private alertify:AlertifyService,private routor:Router) { }
 
   ngOnInit() {
   }
  login(){
    this.authService.login(this.model).subscribe(
      next=>{this.alertify.success("تم الدخول بنجاح")},
-     error=>{this.alertify.error(error)}
+     error=>{this.alertify.error(error)},
+     ()=>{this.routor.navigate(['/members']);}
    )
  }
 
@@ -28,7 +30,12 @@ export class NavComponent implements OnInit {
  }
  LoggedOut(){
 localStorage.removeItem('token');
-this.alertify.message('تم الخروج')
+this.alertify.message('تم الخروج');
+this.routor.navigate(['/home']);
+
+
+
+
  }
  
 }
